c49e22b908cf57970ebd8aaa1ac2bbea4da4d4a7
MatiasBV <matias.barrerav@usm.cl>
2023-05-29 01:55:28 -0400

    Update compras.php
    ahora lo muestra con URL

--- compras.php
+++ compras.php
@@ -1,6 +1,7 @@
 <?php
     include "bd.php";
-    session_start();
+    include "header.php";
+
     
     if(isset($_SESSION['usuario'])){
         $nombreUsuario = $_SESSION['nombre'];
@@ -14,12 +15,22 @@
         while($row = mysqli_fetch_assoc($query)){
             $id_compra = $row['id_compra'];
             $id_producto = $row['id_producto'];
-            $cantidad = $row['cantidad'];
+            $cantidad = $row['cantidad'];            
 
-            echo 'Numero de identificacion de la compra: '.$id_compra.'<br>'.'Numero de identificacion del producto: '.$id_producto.'<br>'.'Cantidad de productos: '.$cantidad.'<br>';
-        
+            if($id_producto >=1000 && $id_producto <2000){
+                $query4 = mysqli_query($conexion, "SELECT Nombre FROM hoteles WHERE id = '$id_producto'");
+                $name = mysqli_fetch_assoc($query4)['Nombre'];
+                echo '<br>HOTELES: <br>';
+                $query2 = mysqli_query($conexion, "SELECT * FROM compras WHERE id_compra = '$id_producto'");
+                echo 'Numero de identificacion de la compra: '.$id_compra.'<br>'.'Nombre de identificacion del producto: '.'<a href="hotelesYpaquetes.php?id='.$id_producto.'">'.$name.'</a><br>';
+            }elseif($id_producto >= 2000){
+                $query4 = mysqli_query($conexion, "SELECT nombre FROM paquetes WHERE id = '$id_producto'");
+                $name = mysqli_fetch_assoc($query4)['nombre'];
+                echo '<br>PAQUETES: <br>';
+                $query2 = mysqli_query($conexion, "SELECT * FROM compras WHERE id_compra = '$id_producto'");
+                echo 'Numero de identificacion de la compra: '.$id_compra.'<br>'.'Producto: '.'<a href="hotelesYpaquetes.php?id='.$id_producto.'">'.$name.'</a><br>';
+                // echo 'Nombre : '.'<a href="hotelesYpaquetes.php?id='.$id_producto.'">'.$nombre_producto.'</a><br>Puntuacion :'.$promedio.'<br>';
+            }
         }
     }
-    echo'chupa chupa'
-
 ?>
